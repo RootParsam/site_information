41599612637be7923d7be54d12d1bd6e683cae75
RootParsam <94349119+RootParsam@users.noreply.github.com>
2022-06-19 18:47:47 +0430

    Update site_information.py

--- site_information.py
+++ site_information.py
@@ -1,7 +1,17 @@
 import builtwith
 from colorama import Fore,init,Back
+
+
+print ("""\n        _)   |              _)            _|                                      |    _)                 
+   __|   |   __|    _ \      |   __ \    |      _ \     __|   __ `__ \     _` |   __|   |    _ \    __ \  
+ \__ \   |   |      __/      |   |   |   __|   (   |   |      |   |   |   (   |   |     |   (   |   |   | 
+ ____/  _|  \__|  \___|     _|  _|  _|  _|    \___/   _|     _|  _|  _|  \__,_|  \__|  _|  \___/   _|  _| """)
+
+print ("""                      Created By RootParsam
+                            instagram : parsam._.mhmdi """)
 init()
-site = input("Enter a Site : ")
+
+site = input("\nEnter a Site : ")
 
 res = builtwith.builtwith("https://"+site)
 
